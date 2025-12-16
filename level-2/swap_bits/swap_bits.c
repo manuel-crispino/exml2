7bb2b8b36ad4d8786d3b8998bc67eb635e57bf6d
ManuelCrispino <manuel.barman96@gmail.com>
2025-12-16 15:34:28 +0100

    updt

--- level-2/swap_bits/swap_bits.c
+++ level-2/swap_bits/swap_bits.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void print_bits(unsigned char octal)
+void print_bits(unsigned char octet)
 {
-	int byte = 0;
+	unsigned char c;
 	int i = 7;
 	while(i >= 0)
 	{
-		octal = octal >> i;	
-		byte = octal & 1;	
-		write(1,&byte,1);
+		c = ((octet >> i) & 1)+ '0';	
+		write(1,&c,1);
 		i--;
 	}
+	write(1,"\n",1);
+}
+
+unsigned char swap_bits(unsigned char octet)
+{
+	int i = 3;	
+	unsigned char res;
+	while(i >= 0)	
+	{
+			res = res << 1;
+			res = res | ((octet >> i) & 1);
+		i--;	
+	}
+	i = 7;
+	while(i >= 4)	
+	{
+			res = res << 1;
+			res = res | ((octet >> i) & 1);
+		i--;	
+	}	
+	return (res);	
 }
 
 int main (void)
 {
-	unsigned char b = 2;
+	unsigned char b = 105;
+	print_bits(b);
+	b = swap_bits(b);
 	print_bits(b);
 	return (0);
 }
